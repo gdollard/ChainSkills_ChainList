b297f21fb79c2eb67ca13b0b75d596eac0b846fe
gdollard <glenn.dollard@gmail.com>
2020-03-22 15:11:41 +0000

    updated ganache script to create txn on node

--- src/js/invokeTest.js
+++ src/js/invokeTest.js
@@ -1,10 +1,19 @@
+/**
+ * This file interacts with various contracts on the Ganache network using web3js
+ */
+
 var Web3 = require('web3');
 var ganacheProvider = new Web3.providers.HttpProvider("http://localhost:7545");
 var web3 = new Web3(ganacheProvider);
-const artifacts = require('../../build/contracts/Inbox.json');
-var contract = require("@truffle/contract");
-let MyContract = contract(artifacts);
-MyContract.setProvider(ganacheProvider);
+const inboxArtifact = require('../../build/contracts/Inbox.json');
+var truffleContract = require("@truffle/contract");
+let inboxContract = truffleContract(inboxArtifact);
+inboxContract.setProvider(ganacheProvider);
+
+const chainListArtifact = require('../../build/contracts/ChainList.json');
+var truffleContract = require("@truffle/contract");
+let chainListContract = truffleContract(chainListArtifact);
+chainListContract.setProvider(ganacheProvider);
 
 
 const ganache_account_0 = '0x207526Be94a4F1DB646a8291Fe0A99327B2338a8';
@@ -15,11 +24,38 @@ const balance = web3.eth.getBalance(ganache_account_0, (err, wei) => {
 
 
 const callGetGreeting = async() => {
-    await MyContract.deployed().then(instance => {
-        instance.getMessage().then(value => {console.log("Data from the contract call: ", value)});
+    await inboxContract.deployed().then(instance => {
+        instance.getMessage().then(value => {console.log("Message from smart contract: ", value)});
     }).then((txnID) => {}).catch(function (err) {
         console.log("Promise Rejected", err)});
 }
 
-console.log("Init Inbox contract..");
-callGetGreeting();
+const callGetNumberOfArticles = async() => {
+    await chainListContract.deployed().then(instance => {
+        instance.getNumberOfArticles().then(value => {console.log("Number of articles: ", value)});
+    }).then((txnID) => {}).catch(function (err) {
+        console.log("Promise Rejected", err)});
+}
+
+const callGetArticlesForSale = async() => {
+    await chainListContract.deployed().then(instance => {
+        instance.getArticlesForSale().then(value => {console.log("Articles for sale: ", value)});
+    }).then((txnID) => {}).catch(function (err) {
+        console.log("Promise Rejected", err)});
+}
+
+const callSellArticle = async() => {
+    await chainListContract.deployed().then(instance => {
+        instance.sellArticle("My TV", "This TV is broken!", web3.utils.toWei("1", "ether"), {from: ganache_account_0, gas: 5000000}).then
+        (value => {console.log("Article submitted for sale : ", value)});
+    }).then((txnID) => {}).catch(function (err) {
+        console.log("Promise Rejected", err)});
+}
+
+
+
+console.log("Calling contract functions..");
+// callGetGreeting();
+//callGetNumberOfArticles();
+//callGetArticlesForSale();
+callSellArticle();
