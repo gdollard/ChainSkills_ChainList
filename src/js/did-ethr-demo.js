1e6715d758a6aa6bdf1fa3dc1eaedecd8c12f837
gdollard <glenn.dollard@gmail.com>
2020-03-27 20:56:32 +0000

    clean up code and use web3 provider only

--- src/js/did-ethr-demo.js
+++ src/js/did-ethr-demo.js
@@ -1,20 +1,19 @@
-//Use 'ethjs-provider-http' instead of 'web3' due to this issue: https://github.com/uport-project/ethr-did/issues/3#issuecomment-413908925
 const HDWalletProvider = require("truffle-hdwallet-provider");
 require('dotenv').config(); //need this module to retrieve the infura mnemonic and API key
 var Web3 = require('web3');
 var HDwalletProvider =  new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY);
 var web3 = new Web3(HDwalletProvider);
-const HttpProvider = require('ethjs-provider-http')
-let provider = new HttpProvider('https://ropsten.infura.io/v3/82fa156d2820477885af5607d839f448')
-//Registery address for ethr did
-const registryAddress = '0xdCa7EF03e98e0DC2B855bE647C39ABe984fcF21B'
-const providerConfig = {rpcUrl: 'https://ropsten.infura.io/v3/82fa156d2820477885af5607d839f448', registry: registryAddress}
+
 const Resolver = require('did-resolver').Resolver
 const getResolver = require('ethr-did-resolver').getResolver
 const EthrDID = require('ethr-did');
 
+//Ethereum DID Registery address 
+const ethereumDIDRegistryAddress = '0xdCa7EF03e98e0DC2B855bE647C39ABe984fcF21B'
+const ropsten_0_address = '0xEdAA87f3a3096bc7C0CE73971b1c463f20Cf3Af5';
+const ropsten_1_address = '0xB72fD1f1cC6ecbE44270a5E235e81d768cf1BF86';
+
 let test = async () => {
-    
 
     //Generating Eth keyPair {address, private key}
     const keypair = EthrDID.createKeyPair()
@@ -23,24 +22,24 @@ let test = async () => {
     //Generating Ethr DID
     const ethrDid = new EthrDID({
         ...keypair,
-        HDwalletProvider,
-        registry: registryAddress
+        web3,
+        registry: ethereumDIDRegistryAddress
     })
     
-
-    let did = ethrDid.did
+    // the full DID compatible string 
+    let didString = ethrDid.did
 
     //Registering Ethr Did To Resolver
     const ethrDidResolver = getResolver({
         web3,
-        registry: registryAddress,
+        registry: ethereumDIDRegistryAddress,
     })
     
     // create a DID resolver based on the ethr DID resolver
     const didResolver = new Resolver(ethrDidResolver)
     
     // resolve the DID document for the given DID identity
-    didResolver.resolve(did).then(doc => {console.log("XXX", doc)
+    didResolver.resolve(didString).then(doc => {console.log("DID Document", doc)
         process.exit()})
 }
 
