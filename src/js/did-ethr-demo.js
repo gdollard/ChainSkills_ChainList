a44de7f7599b984a70c2c5563ee7e6049f1c136c
gdollard <glenn.dollard@gmail.com>
2020-03-30 12:06:21 +0100

    clean up

--- src/js/did-ethr-demo.js
+++ src/js/did-ethr-demo.js
@@ -20,34 +20,30 @@ let ethrDid
 
 let createEthrDID = async () => {
 
-    const keypairAlt = {
-        address: process.env.EthrDID_ADDRESS,
-        privateKey: process.env.EthrDID_PKEY
-    }
     // get the accounts from the Ropsten network
     const accounts =  await web3.eth.getAccounts()
     const keyPair = {
-        address: accounts[0],
-        privateKey: process.env.ACCOUNT_0_PKEY
+        // the keypair below are ones from a Ropsten account but they seem to fail for JWT creation/verification in this scenario
+        //address: accounts[0],
+        //privateKey: process.env.ACCOUNT_0_PKEY
+
+        // The keypair below were created by EthrDID.createKeyPair()
+        address: process.env.EthrDID_ADDRESS,
+        privateKey: process.env.EthrDID_PKEY
     }
 
-    console.log("keypairAlt ", keypairAlt)
-    console.log("keyPair", keyPair)
-    
-    const keyPair_address = 'did:ethr:0xEdAA87f3a3096bc7C0CE73971b1c463f20Cf3Af5'
-
     // create the signer from the private key
-    const signer = SimpleSigner(keypairAlt.privateKey)
+    const signer = SimpleSigner(keyPair.privateKey)
 
     //Generating Ethr DID
     ethrDid = new EthrDID({
-        ...keypairAlt,
+        ...keyPair,
         provider: web3,
         registry: ethereumDIDRegistryAddress
     })
 
     // this is the complete DID ID (the ethereum address formatted as an ether DID address )
-    const didAddress = ethrDid.did//'did:ethr:' + keypairAlt.address
+    const didAddress = ethrDid.did
     
     //Registering Ethr Did To Resolver
     const ethrDidResolver = getResolver({
@@ -57,71 +53,39 @@ let createEthrDID = async () => {
     
     // create a DID resolver based on the ethr DID resolver
     const didResolver = new Resolver(ethrDidResolver)
-
     
 
     //const helloJWT = await ethrDid.signJWT({claims: { name: 'Joe Lubin' }})
     //console.log("Data from signJWT: ", helloJWT)
    // console.log("Decoded JWT created from ethrDID:", didJWT.decodeJWT(helloJWT))
-    
-    
-    let jwt = '';
-    
+        
     const theJWT = await didJWT.createJWT({ aud: didAddress, exp: 1957463421, name: 'uPort Developer' },
          { alg: `ES256K-R`, issuer: didAddress, signer }).catch(error => {
              console.log("Error creating/verifying JWT:", error.message)
              process.exit()
          })
     
-   
+    // decode it just for debug purposes
     didJWT.decodeJWT(theJWT)
 
     // when verifying the token I need to pass the audience argument if it was specified as the 'aud' argument in the createJWT call
     didJWT.verifyJWT(theJWT, {resolver: didResolver, audience: didAddress }).then((verifiedResponse) => {
     console.log("Verified response from verifyJWT: ", verifiedResponse)
     }).catch(error => {
-        console.log("lslkdjlksjdskljdsjd", error.message)
+        console.log("Error trying to verify JWT: ", error.message)
         process.exit()
     })
 
     // resolve the DID document for the given DID identity
     didResolver.resolve(ethrDid.did).then(doc => {
-        console.log("DID Document", doc)
+        console.log("Resolved DID Document", doc)
         
         })
 }
 
-/**
- * Sign and verify a JWT.
- */
-let signJWT = async () => {
-    
-
-    
-
-    let verifiedRespone = {};
-    // pass the JWT from step 1 & 2
-    didJWT.verifyJWT(helloJWT, {resolver: didResolver, audience: 'did:ethr:0xf3beac30c498d9e26865f34fcaa57dbb935b0d74'}).then((response) =>
-        { verifiedRespone = response });
-    console.log("TTTTTT", verifiedRespone);
-    
-
-
-
-    //await ethrDid.createSigningDelegate().catch(error => {console.log("Error creating a signing delegate", error.message)})
-    const {payload, issuer} = await verifyJWT(helloJWT).catch(error => {console.log("Error when verifying JWT: ", error)})
-    //const {payload, issuer} = await ethrDid.verifyJWT(helloJWT).then(data => {console.log("GGGG ", data)}).catch(error => {console.log("Error when verifying JWT: ", error)})
-    // payload contains the JavaScript object that was signed together with a few JWT specific attributes
-    //console.log("Verified Payload: ", payload)
-    // Issuer contains the DID of the signing identity
-    //console.log("iSSUER: ", issuer)
-
-}
 
 let doStuff = async () => {
-    await createEthrDID()
-   
-    //signJWT()
+    await createEthrDID() 
 }
 
 doStuff()
