b15ff363501b471e8f3044da229f27a0e42ff56d
gdollard <glenn.dollard@gmail.com>
2020-04-04 15:36:57 +0100

    latest additions

--- src/js/TrustAnchor.js
+++ src/js/TrustAnchor.js
@@ -7,7 +7,8 @@ const DidRegistryContract = require('ethr-did-registry');
 const Contract = require("@truffle/contract");
 const truffleDIDRegistryContract = Contract(DidRegistryContract);
 truffleDIDRegistryContract.setProvider(HDwalletProvider);
-
+const EthrDID = require('ethr-did');
+require('ethr-did');
 const ETHEREUM_DID_REGISTRY_ADDRESS = "0xdca7ef03e98e0dc2b855be647c39abe984fcf21b";
 
 /**
@@ -18,10 +19,34 @@ const requestDataAccess = async (accountAddress) => {
 
     //verify the owner of the identity by calling the Ethereum registry contract using web3
     let DidReg = new web3.eth.Contract(DidRegistryContract.abi, ETHEREUM_DID_REGISTRY_ADDRESS);
-    let idOwner = await DidReg.methods.identityOwner(accountAddress).call();
-    console.log("Owner is: ", idOwner);
-
+    let idOwner; 
+    DidReg.methods.identityOwner(accountAddress).call().then(result => {
+        if(result === accountAddress) {
+            idOwner = true;
+            return;
+        }
+        idOwner = false;
+        return;
+    })
+    
+    return idOwner;
 }
+
+
+/**
+ * Create a DID 
+ */
+const createDID = () => {
+    
+    const keypair = EthrDID.createKeyPair();
+
+    ethrDid = new EthrDID({
+        ...keypair,
+        provider: web3,
+        registry: ETHEREUM_DID_REGISTRY_ADDRESS
+    });
+    return ethrDid;
+};
 
 /**
  * 
@@ -35,6 +60,5 @@ const requestDataAccessUsingTruffleContract = async (accountAddress) => {
     console.log("Owner: ", idOwner);
 };
 
-//requestDataAccess(ropsten_0_address);
+module.exports = {createDID, requestDataAccess};
 
-exports.requestDataAccess = requestDataAccess;
--- src/js/alice.js
+++ src/js/alice.js
@@ -1,4 +1,14 @@
 const trustAnchor = require('./TrustAnchor').requestDataAccess;
+const createDID = require('./TrustAnchor').createDID;
 
 const ropsten_0_address = '0xEdAA87f3a3096bc7C0CE73971b1c463f20Cf3Af5';
-trustAnchor(ropsten_0_address);
+trustAnchor(ropsten_0_address).then(result => {
+    console.log("Good to go...");
+    process.exit(0);
+}).catch(error => {
+    console.log("Nope.");
+    process.exit(1);
+});
+
+
+myDID = createDID();
